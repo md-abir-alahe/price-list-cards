2e3f5dbad9623762be143c7766f0688425b98fa9
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-05 21:33:37 +0600

    axios and recharts explored

--- src/components/PhoneBar/PhoneBar.jsx
+++ src/components/PhoneBar/PhoneBar.jsx
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const PhoneBar = () => {
+  const [phones, setPhones] = useState([]);
 
-    const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(res=>res.json())
+    // .then(data=>console.log(data))
 
-    useEffect(()=>{
-        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        // .then(res=>res.json())
-        // .then(data=>console.log(data))
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const loadedData = data.data.data;
+        console.log(loadedData);
+        const phonesData = loadedData.map((phone) => {
+          const parts = phone.slug.split("-");
+          const price = parseInt(parts[1]);
+          const phoneInfo = {
+            name: phone.phone_name,
+            price: price,
+          };
+          return phoneInfo;
+        });
+        console.log(phonesData);
+        setPhones(phonesData);
+      });
+  }, []);
 
-        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(data => {
-            const loadedData = data.data.data;
-            console.log(loadedData);
-            const phonesData = loadedData.map(phone =>{
-                
-            })
-
-        })
-    },[])
-
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+        <BarChart width={1000} height={400} data={phones}>
+          <Bar dataKey="price" fill="#8884d8" />
+          <XAxis dataKey="name"></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+          <Legend></Legend>
+          <Cell>
+          </Cell>
+        </BarChart>
+    </div>
+  );
 };
 
 export default PhoneBar;
